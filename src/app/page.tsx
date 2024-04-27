a38771bc5987af518ac3fe76277f0c253c85d9a1
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-04-27 16:36:29 +0600

    displaying todays temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
+import Container from "@/components/Container"
 import NavBar from "@/components/NavBar"
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius"
 import axios from "axios"
+import { format } from "date-fns"
+import parseISO from "date-fns/parseISO"
 import Image from "next/image"
 import { useQuery } from "react-query"
 
@@ -71,7 +75,7 @@ export default function Home() {
     }
   )
 
-  console.log("data ", data)
+  const today = data?.list[0]
 
   if (isLoading)
     return (
@@ -83,6 +87,43 @@ export default function Home() {
   return (
     <div className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
       <NavBar />
+      <main className='px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4'>
+        <section>
+          <div>
+            <h2 className='flex gap-1 text-2xl items-end'>
+              <p>{format(parseISO(today?.dt_txt ?? ""), "EEEE")}</p>
+              <p className='text-lg'>
+                ({format(parseISO(today?.dt_txt ?? ""), "dd.MM.yyyy")})
+              </p>
+            </h2>
+            <Container className=' gap-10 px-6 items-center'>
+              <div className='flex flex-col px-4'>
+                <span className='text-5xl'>
+                  {convertKelvinToCelsius(today?.main.temp ?? 0)}°
+                </span>
+                <p className='text-xs space-x-1 whitespace-nowrap'>
+                  <span> Feels Like </span>
+                  <span>
+                    {convertKelvinToCelsius(today?.main.feels_like ?? 0)}°
+                  </span>
+                </p>
+                <p className='text-xs space-x-2'>
+                  <span>
+                    {convertKelvinToCelsius(today?.main.temp_min ?? 0)}
+                    °↓{" "}
+                  </span>
+                  <span>
+                    {" "}
+                    {convertKelvinToCelsius(today?.main.temp_max ?? 0)}
+                    °↑
+                  </span>
+                </p>
+              </div>
+            </Container>
+          </div>
+        </section>
+        <section></section>
+      </main>
     </div>
   )
 }
